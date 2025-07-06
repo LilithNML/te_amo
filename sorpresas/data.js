520935904f3496490fb5f0430bf2d63cec4dc11f
LilithNML <lilithmorningstar468@gmail.com>
2025-07-06 06:43:17 -0600

    Update data.js

--- sorpresas/data.js
+++ sorpresas/data.js
@@ -1,12 +1,12 @@
 const codes = {
-"VUQGSRNBBKUWSWTRWHJCQDNWDVYYPA": "downloads/contrasenas_te_amo.PDF",
-"MWKBAEFZZSTLCDFHQSVCSNDUFPEMYV": "downloads/contrasenas_img.PDF",
-"FJLPCXLHEUXGSUV": "downloads/img_01.jpg.enc",
-"LDURAZNLSXFBWBP": "downloads/img_02.jpg.enc",
-"PKSHUEBQPMMHPMD": "downloads/img_03.jpg.enc",
-"JCNHYTZTDGTLFJD": "downloads/img_04.jpg.enc",
-"VRSVSBZNZXEFLTW": "downloads/img_05.jpg.enc",
-"NZECHLNSQELSPAE": "downloads/img_06.jpg.enc",
-"FBHAXUHCDVSGJCQ": "downloads/img_07.jpg.enc",
-"QUESO123": "downloads/todos_tus_codigos.PDF"
+"vuqgsrnbbkuwswtrwhjcqdnwdvyypa": "downloads/contrasenas_te_amo.PDF",
+"mwkbaefzzstlcdfhqsvcsndufpemyv": "downloads/contrasenas_img.PDF",
+"fjlpcxlheuxgsuv": "downloads/img_01.jpg.enc",
+"lduraznlsxfbwbp": "downloads/img_02.jpg.enc",
+"pkshuebqpmmhpmd": "downloads/img_03.jpg.enc",
+"jcnhytztdgtlfjd": "downloads/img_04.jpg.enc",
+"vrsvsbznzxefltw": "downloads/img_05.jpg.enc",
+"nzechlnsqelspae": "downloads/img_06.jpg.enc",
+"fbhaxuhcdvsgjcq": "downloads/img_07.jpg.enc",
+"teamo3000000": "downloads/todos_tus_codigos.PDF"
 };
