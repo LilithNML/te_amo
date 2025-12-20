e3178edb7dea6056ba51029b8708a61a4d0dda1d
LilithNML <lilithmorningstar468@gmail.com>
2025-12-20 00:47:31 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-/** main.js */
+/**
+ * main.js
+ * Punto de entrada principal.
+ */
 import { UIManager } from './modules/uiManager.js';
 import { AudioManager } from './modules/audioManager.js';
 import { GameEngine } from './modules/gameEngine.js';
 
 document.addEventListener("DOMContentLoaded", () => {
+    // Inicializar Sistemas
     const ui = new UIManager();
     const audio = new AudioManager(ui);
     const game = new GameEngine(ui, audio);
